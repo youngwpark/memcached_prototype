640a7fa2ee6a1efc39899c0706e108dc84ab37a2
Terri Park <terripark@Terris-MacBook-Pro.local>
2016-07-26 17:41:05 -0700

    11

--- src/OppGenerator.java
+++ src/OppGenerator.java
@@ -1,4 +1,43 @@
+import java.lang.InterruptedException;
+import java.lang.Thread;
+import java.lang.Runnable;
 
-public class OppGenerator {
+public class OppGenerator 
+{
 
+	private int counter = 0;
+	private int timerInterval = 10;
+	
+	public OppGenerator()
+	{
+		
+	}
+	
+	public void generateOppDataSet()
+	{
+		
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() 
+			{
+				// TODO Auto-generated method stub
+				while (counter < 100)
+				{
+					try
+					{
+						Thread.sleep(timerInterval * 1000);
+					}
+					catch (InterruptedException ex)
+					{
+						ex.printStackTrace();
+					}
+					counter++;
+				}
+			}
+		});
+		
+		//t.setDaemon(true);
+		t.start();
+	}
+	
 }
